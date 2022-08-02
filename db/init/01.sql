fda43ed1b6d109e5d27f5caa38e76f5a97c1fefd
brenoSN <99511303+brenoSN@users.noreply.github.com>
2022-08-02 20:56:50 -0300

    initial commit

---
+++ db/init/01.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS os_controller;
+USE os_controller;
+
+CREATE TABLE IF NOT EXISTS task_status (
+  id INT(2) NOT NULL,
+  status VARCHAR(11),
+  PRIMARY KEY (id)
+);
+
+insert into task_status (id, status) values (1, 'BACKLOG');
+insert into task_status (id, status) values (2, 'WORKING');
+insert into task_status (id, status) values (3, 'FIXING');
+insert into task_status (id, status) values (4, 'DONE');
+insert into task_status (id, status) values (5, 'PAUSED');
+
+CREATE TABLE IF NOT EXISTS task (
+  id BIGINT NOT NULL,
+  name VARCHAR(150) NOT NULL,
+  description VARCHAR(250),
+  creationDate DATE NOT NULL,
+  lastEditDate DATE NOT NULL,
+  time INT(4) NOT NULL,
+  status INT(2) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (status) REFERENCES task_status(id)
+);
+
+CREATE TABLE IF NOT EXISTS user (
+  id INT NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  UNIQUE (name),
+  PRIMARY KEY (id)
+);
